89a560862b73d280a323b2039d490ebee5d9578b
Elton Santos <ecsantos.developer@gmail.com>
2026-06-02 21:35:00 -0300

    feat(models): define corrida and gasto database structures

---
+++ internal/models/corrida.go
@@ -0,0 +1,22 @@
+package models
+
+import "gorm.io/gorm"
+
+type Corrida struct {
+	gorm.Model
+	UsuarioID           uint    `json:"usuario_id"`
+	VeiculoAtivo        string  `json:"veiculo_ativo"` 
+	ValorRecebido       float64 `json:"valor_recebido"`
+	TipoPagamento       string  `json:"tipo_pagamento"` 
+	DistanciaPercorrida float64 `json:"distancia_km"`
+	DuracaoMinutos       int     `json:"duracao_min"`
+	HorarioCorrida      string  `json:"horario_corrida"` 
+	Plataforma          string  `json:"plataforma"`      
+}
+
+type Gasto struct {
+	gorm.Model
+	UsuarioID uint    `json:"usuario_id"`
+	Descricao string  `json:"descricao"` 
+	Valor     float64 `json:"valor"`
+}
